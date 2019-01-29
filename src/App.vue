adec16b85eade5722ab6070742dc4b4e412cc472
Aman <rumman.djuice1@gmail.com>
2019-01-29 18:24:56 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <template>
     <div class="container">
-    
+
+        <quote-grid v-bind:quotes="quotes"></quote-grid>
     </div>
 </template>
 
 <script>
+    import QuoteGrid from './components/QuoteGrid.vue';
+
     export default {
-        
+        components:{
+            'quote-grid':QuoteGrid,
+
+        },
+        data(){
+            return{
+              quotes:['test quote'],
+              maxQuotes:10,
+            };
+        },
+
+
     }
 </script>
 
---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="row">
+        <quote v-for="x in quotes">{{x}}</quote>
+    </div>
+</template>
+
+<script>
+    import Quote from './Quote.vue'
+    export default {
+        components:{
+            'quote':Quote,
+        },
+        props:['quotes'],
+    }
+</script>
+
+<style scoped>
+
+</style>
