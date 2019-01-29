9eddf622c9ab7e34011b5d1ecc764aab54085428
Aman <rumman.djuice1@gmail.com>
2019-01-29 18:31:21 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,26 @@
 <template>
     <div class="container">
-
+        <new-quote v-on:get-quote="quotes.push($event)"></new-quote>
         <quote-grid v-bind:quotes="quotes"></quote-grid>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="alert alert-info">Click on a quote to delete it</div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
     import QuoteGrid from './components/QuoteGrid.vue';
-
+    import NewQuote from './components/NewQuote.vue';
     export default {
         components:{
             'quote-grid':QuoteGrid,
-
+            'new-quote':NewQuote,
         },
         data(){
             return{
-              quotes:['test quote'],
+              quotes:[],
               maxQuotes:10,
             };
         },
